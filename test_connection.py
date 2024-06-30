3097d8f397bbb1c4796341737b4bec27ad5b7f7f
Huy Nguyen <nguyenhoaihuy92@gmail.com>
2024-06-30 16:37:13 -0700

    get rcp_address of cassandra request

--- test_connection.py
+++ test_connection.py
@@ -1,4 +1,12 @@
 from cassandra.cluster import Cluster
 
-cluster = Cluster(['172.18.0.1'], port=30183)  # 'cassandra' is the Kubernetes service name
+cluster = Cluster(['172.18.0.4'], port=30183)  # 'cassandra' is the Kubernetes service name
 session = cluster.connect()
+# Query system tables to get rpc_address
+query = "SELECT rpc_address FROM system.local"
+
+# Execute query
+rows = session.execute(query)
+# Print rpc_address for each node
+for row in rows:
+    print(f"RPC Address: {row.rpc_address}")
